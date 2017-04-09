321f135de27935406611a53d64586bc56bf029cc
vidyavnv <vidya.vnv@gmail.com>
2017-04-08 21:46:12 -0400

    Improve 2 space indentation

--- sparsenet.py
+++ sparsenet.py
@@ -6,50 +6,49 @@ from matplotlib import cm
 import fista
 
 def sparsenet(IMAGES, patch_dim=64, neurons=128, lambdav=0.1, eta=6.0, num_trials=100, batch_size=100, border=4, inference='fista'):
-  """
-  N: # Inputs
-  M: # Outputs
-  lambdav: Sparsity Constraint
-  eta: Learning Rate
-  num_trials: Learning Iterations
-  batch_size: Batch size per iteration
-  border: Border when extracting image patches
-  Inference: 'lca' or 'fista'  
-  """
-  # IMAGES = scipy.io.loadmat('./IMAGES.mat')
-  # IMAGES = IMAGES['IMAGES']
+    """
+    N: # Inputs
+    M: # Outputs
+    lambdav: Sparsity Constraint
+    eta: Learning Rate
+    num_trials: Learning Iterations
+    batch_size: Batch size per iteration
+    border: Border when extracting image patches
+    Inference: 'lca' or 'fista'  
+    """
+    # IMAGES = scipy.io.loadmat('./IMAGES.mat')
+    # IMAGES = IMAGES['IMAGES']
 
-  (num_images, imsize, imsize) = np.shape(IMAGES)
+    (num_images, imsize, imsize) = np.shape(IMAGES)
 
-  sz = np.sqrt(patch_dim)
-  eta = eta / batch_size
+    sz = np.sqrt(patch_dim)
+    eta = eta / batch_size
 
-  # Initialize basis functions
-  Phi = np.random.randn(patch_dim, neurons)
-  Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis = 0))))
+    # Initialize basis functions
+    Phi = np.random.randn(patch_dim, neurons)
+    Phi = np.dot(Phi, np.diag(1/np.sqrt(np.sum(Phi**2, axis = 0))))
 
-  I = np.zeros((patch_dim,batch_size))
+    I = np.zeros((patch_dim,batch_size))
 
-  for t in range(num_trials):
+    for t in range(num_trials):
 
-    # Choose a random image
-    imi = np.ceil(num_images * random.uniform(0, 1))
+        # Choose a random image
+        imi = np.ceil(num_images * random.uniform(0, 1))
 
     for i in range(batch_size):
-      r = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
-      c = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
-      # print(imi-1)
-      np.reshape(IMAGES[imi-1, r:r+sz, c:c+sz], patch_dim, 1)
-      I[:,i] = np.reshape(IMAGES[imi-1, r:r+sz, c:c+sz], patch_dim, 1)
+        r = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
+        c = border + np.ceil((imsize-sz-2*border) * random.uniform(0, 1))
+        # print(imi-1)
+        I[:,i] = np.reshape(IMAGES[imi-1, r:r+sz, c:c+sz], patch_dim, 1)
 
     # Coefficient Inference
     if inference == 'lca':
-      ahat = sparsify(I, Phi, lambdav)
+        ahat = sparsify(I, Phi, lambdav)
     elif inference == 'fista':
-      ahat = fista.fista(I, Phi, lambdav, max_iterations=50)
+        ahat = fista.fista(I, Phi, lambdav, max_iterations=50)
     else:
-      print "Invalid inference option"
-      return
+        print "Invalid inference option"
+        return
 
     # Calculate Residual Error
     R = I-np.dot(Phi, ahat)
@@ -61,14 +60,14 @@ def sparsenet(IMAGES, patch_dim=64, neurons=128, lambdav=0.1, eta=6.0, num_trial
 
     # Plot every 100 iterations
     if np.mod(t,100) == 0:
-      print "Iteration " + str(t)
-      side = np.sqrt(neurons)
-      image = np.zeros((sz*side+side,sz*side+side))
-      for i in range(side.astype(int)):
+        print "Iteration " + str(t)
+        side = np.sqrt(neurons)
+        image = np.zeros((sz*side+side,sz*side+side))
+        for i in range(side.astype(int)):
         for j in range(side.astype(int)):
-          patch = np.reshape(Phi[:,i*side+j],(sz,sz))
-          patch = patch/np.max(np.abs(patch))
-          image[i*sz+i:i*sz+sz+i,j*sz+j:j*sz+sz+j] = patch
+            patch = np.reshape(Phi[:,i*side+j],(sz,sz))
+            patch = patch/np.max(np.abs(patch))
+            image[i*sz+i:i*sz+sz+i,j*sz+j:j*sz+sz+j] = patch
 
       plt.imshow(image, cmap=cm.Greys_r, interpolation="nearest")
 
